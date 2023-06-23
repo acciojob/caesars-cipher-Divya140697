4d2e86c790fb5494ccd508b18e9395d0e7d0e951
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-06-23 06:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,12 +30,33 @@ const lookup = {
   "?": "?",
   ",": ",",
 };
+function rot13(encodedString) {
+  // Helper function to decode a single character
+  function decodeChar(char) {
+    // Check if the character is within the range of uppercase letters
+    if (/[A-Z]/.test(char)) {
+      // Convert the character code to a value between 0 and 25 (A=0, B=1, etc.)
+      const charCode = char.charCodeAt(0) - 65;
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+      // Perform the ROT13 decoding by shifting the character code by 13 positions
+      // and wrapping around if it goes beyond the range of 26
+      const decodedCharCode = (charCode + 13) % 26;
 
-  return; //return decodedArr
+      // Convert the decoded character code back to its corresponding uppercase letter
+      const decodedChar = String.fromCharCode(decodedCharCode + 65);
+
+      return decodedChar;
+    } else {
+      // If the character is not an uppercase letter, return it as is
+      return char;
+    }
+  }
+
+  // Split the encoded string into an array of characters, decode each character,
+  // and join them back into a string
+  const decodedString = encodedString.split('').map(decodeChar).join('');
+
+  return decodedString;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
